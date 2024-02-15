4571018f27ec582efd3d30a493f9e658bfd0d009
mimisosorara <igi712.mgj@gmail.com>
2024-02-15 07:58:33 +0700

    datatypes update : output later

--- src/DataTypes/DataTypes.java
+++ src/DataTypes/DataTypes.java
@@ -8,27 +8,32 @@ public class DataTypes {
         Scanner q = new Scanner(System.in);
         System.out.println("How many times would you enter a number?");
         T = q.nextInt();
-        long n; // deklarasi variabel n untuk menampung
+        String[] n = new String[100]; // deklarasi array n untuk menampung
         for (int i = 0; i < T; i++) {
             Scanner a = new Scanner(System.in); // menyiapkan scanner t untuk membaca input user
             System.out.println("Enter a number");
-            if (a.hasNextLong()) { // mengecek
-                n = a.nextLong();
-                System.out.println(n + " can be fitted in");
-                if (Byte.MIN_VALUE <= n && n <= Byte.MAX_VALUE) {
+            n[i] = a.nextLine();
+        }
+        long N;
+        for (int i = 0; i < T; i++) {
+            try {
+                N = Long.parseLong(n[i]);
+            } catch (Exception e) {
+                System.out.println(n[i] + " can't be fitted anywhere.");
+                continue;
+            }
+                System.out.println(N + " can be fitted in");
+                if (Byte.MIN_VALUE <= N && N <= Byte.MAX_VALUE) {
                     System.out.println("* byte");
                 }
-                if (Short.MIN_VALUE <= n && n <= Short.MAX_VALUE) {
+                if (Short.MIN_VALUE <= N && N <= Short.MAX_VALUE) {
                     System.out.println("* short");
                 }
-                if (Integer.MIN_VALUE <= n && n <= Integer.MAX_VALUE) {
+                if (Integer.MIN_VALUE <= N && N <= Integer.MAX_VALUE) {
                     System.out.println("* int");
                 }
                 System.out.println("* long");
-            } else {
-                String nerror = a.nextLine();
-                System.out.println(nerror + " can't be fitted anywhere.");
-            }
         }
+
     }
 }
